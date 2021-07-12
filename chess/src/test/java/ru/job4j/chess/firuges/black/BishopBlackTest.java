07fa3b18c9df5513d91b3aba8615afa71d3f7c40
noslav7 <noslav7@gmail.com>
2021-07-12 12:06:24 +0500

    BishopBlackTest.java

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.junit.Assert.*;
+
+public class BishopBlackTest {
+
+    @Test
+    public void correctPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.position();
+        assertTrue(bishopBlack.position() == Cell.C1);
+    }
+
+    @Test
+    public void preciseCopy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Figure anotherBishopBlack = bishopBlack.copy(Cell.F1);
+        assertTrue(anotherBishopBlack.position() == Cell.F1);
+    }
+
+    @Test
+    public void way(Cell dest) {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G5);
+        assertTrue();
+    }
+}
