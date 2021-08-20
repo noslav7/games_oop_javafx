19d9ab7bd5b4c8ee5f30433a2e9cfa62f8aabb90
noslav7 <noslav7@gmail.com>
2021-08-20 11:42:16 +0500

    2. Каркас шахматной доски [#792 #175692]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,23 +3,52 @@ package ru.job4j.chess;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class  LogicTest {
 
-   @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        try {
-            logic.move(Cell.C1, Cell.H6);
-        } catch (FigureNotFoundException ffe) {
-            ffe.printStackTrace();
-        } catch (OccupiedCellException oce) {
-            oce.printStackTrace();
-        } catch (ImpossibleMoveException ime) {
-            ime.printStackTrace();
+    @Test(expected = FigureNotFoundException.class)
+    private int findBy(Cell cell) throws FigureNotFoundException {
+        final Figure[] figures = new Figure[32];
+        for (int index = 0; index != figures.length; index++) {
+            Figure figure = figures[index];
+            if (figure != null && figure.position().equals(cell)) {
+                return index;
+            }
+        }
+        throw new FigureNotFoundException();
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    private boolean free(Cell[] steps) throws OccupiedCellException {
+        final Figure[] figures = new Figure[32];
+        for (int j = 0; j < figures.length; j++) {
+            for (int k = 0; k < steps.length; k++) {
+                if (figures[j] != null && figures[j].equals(steps[k])) {
+                    throw new OccupiedCellException();
+                }
+            }
+        }
+        return true;
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void way(Cell dest) {
+        Cell position = null;
+        BishopBlack bishB = new BishopBlack(Cell.C8);
+        if (!bishB.isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
         }
     }
-}
+
+        @Test
+        public void move ()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            Logic logic = new Logic();
+            logic.add(new BishopBlack(Cell.C1));
+            logic.move(Cell.C1, Cell.H6);
+        }
+    }
