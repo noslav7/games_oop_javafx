26e40e2face837d3c4c0f9e9bebeea0f60d79086
Valentin Rachkov <noslav7@gmail.com>
2021-08-31 14:34:19 +0500

    2. Каркас шахматной доски [#792 #175692]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,42 +7,26 @@ import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class  LogicTest {
+    Cell position = Cell.C8;
+    BishopBlack blackBishop = new BishopBlack(position);
 
     @Test(expected = FigureNotFoundException.class)
-    private int findBy(Cell cell) throws FigureNotFoundException {
-        final Figure[] figures = new Figure[32];
-        for (int index = 0; index != figures.length; index++) {
-            Figure figure = figures[index];
-            if (figure != null && figure.position().equals(cell)) {
-                return index;
-            }
+    public void whenFigureNotFound() throws FigureNotFoundException {
+        Logic chessGame = new Logic();
+            chessGame.findBy(position);
         }
-        throw new FigureNotFoundException();
-    }
 
     @Test(expected = OccupiedCellException.class)
-    private boolean free(Cell[] steps) throws OccupiedCellException {
-        final Figure[] figures = new Figure[32];
-        for (int j = 0; j < figures.length; j++) {
-            for (int k = 0; k < steps.length; k++) {
-                if (figures[j] != null && figures[j].equals(steps[k])) {
-                    throw new OccupiedCellException();
-                }
-            }
-        }
-        return true;
+    public void whenCellOcupied() throws OccupiedCellException {
+        Cell[] steps = blackBishop.way(position);
+        Logic chessGames = new Logic();
+            chessGames.free(steps);
     }
 
     @Test(expected = ImpossibleMoveException.class)
-    public void way(Cell dest) {
-        Cell position = null;
-        BishopBlack bishB = new BishopBlack(Cell.C8);
-        if (!bishB.isDiagonal(position, dest)) {
-            throw new ImpossibleMoveException(
-                    String.format("Could not move by diagonal from %s to %s", position, dest)
-            );
+    public void whenImpossibleMove() {
+        blackBishop.way(position);
         }
-    }
 
         @Test
         public void move ()
